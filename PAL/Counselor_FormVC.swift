997f2d0c82b374f5b25cd75353c7574bd76fe227
admin <admin@tom-lius-macbook-pro.local>
2017-07-25 10:41:34 -0400

    Linked Forms to VC

--- PAL/Counselor_FormVC.swift
+++ PAL/Counselor_FormVC.swift
@@ -2,7 +2,7 @@
 //  Counselor_FormVC.swift
 //  PAL
 //
-//  Created by admin on 7/24/17.
+//  Created by admin on 7/25/17.
 //  Copyright © 2017 Priyanka Shah. All rights reserved.
 //
 
@@ -11,31 +11,39 @@ import UIKit
 class Counselor_FormVC: UIViewController {
     
     //MARK: Variables
-    @IBOutlet weak var questionLabel: UILabel!
-    @IBOutlet weak var strongDisagree: UILabel!
-    @IBOutlet weak var disagree: UILabel!
-    @IBOutlet weak var notSure: UILabel!
-    @IBOutlet weak var agree: UILabel!
-    @IBOutlet weak var strongAgree: UILabel!
+    
+    var counselorQuestions: [String] = [""]
+    @IBOutlet weak var questionsLabel: UILabel!
 
-    @IBOutlet weak var sd_switch: UISwitch!
-    @IBOutlet weak var d_switch: UISwitch!
-    @IBOutlet weak var ns_switch: UISwitch!
-    @IBOutlet weak var a_switch: UISwitch!
-    @IBOutlet weak var sa_switch: UISwitch!
-    
-    @IBOutlet weak var submit_butt: UIButton!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func strongDisagree(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func disagreeButt(_ sender: UIButton) {
+    }
+    
+    @IBAction func notSureButt(_ sender: UIButton) {
+    }
+    
+    @IBAction func agreeButt(_ sender: UIButton) {
+    }
+    
+    @IBAction func strongAgreeButt(_ sender: UIButton) {
+    }
+    
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
 }
--- PAL/Student_FormVC.swift
+++ PAL/Student_FormVC.swift
@@ -2,7 +2,7 @@
 //  Student_FormVC.swift
 //  PAL
 //
-//  Created by admin on 7/24/17.
+//  Created by admin on 7/25/17.
 //  Copyright © 2017 Priyanka Shah. All rights reserved.
 //
 
@@ -11,32 +11,33 @@ import UIKit
 class Student_FormVC: UIViewController {
     
     //MARK: Variables
-    @IBOutlet weak var questionLabel: UILabel!
-    @IBOutlet weak var strongDisagree: UILabel!
-    @IBOutlet weak var disagree: UILabel!
-    @IBOutlet weak var notSure: UILabel!
-    @IBOutlet weak var agree: UILabel!
-    @IBOutlet weak var strongAgree: UILabel!
-    
-    @IBOutlet weak var sd_switch: UISwitch!
-    @IBOutlet weak var d_switch: UISwitch!
-    @IBOutlet weak var ns_switch: UISwitch!
-    @IBOutlet weak var a_switch: UISwitch!
-    @IBOutlet weak var sa_switch: UISwitch!
-    
-    @IBOutlet weak var submit_butt: UIButton!
+    var studentQuestions: [String] = [""]
+    @IBOutlet weak var questionsLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func strongDisagreeButt(_ sender: UIButton) {
+    }
+    
+    @IBAction func disagreeButt(_ sender: UIButton) {
+    }
 
+    @IBAction func notSureButt(_ sender: UIButton) {
+    }
+    
+    @IBAction func agreeButt(_ sender: UIButton) {
+    }
+    
+    @IBAction func strongAgreeButt(_ sender: UIButton) {
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-   
 }
